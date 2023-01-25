aa7be979c13f161970c512d53551469b37398000
ApBertran <ray200606@gmail.com>
2023-01-25 13:43:48 -0800

    Added Web GUI Development

---
+++ Source/Web GUI Testing/README.md
@@ -0,0 +1,12 @@
+## Important Info
+### Electron Version:
+electron@v22.1.0
+### Required Stuff:
+* need node.js for electron
+* run `npm start` in the correct directory to start gui
+
+## Useful Links
+https://www.electronjs.org/docs/latest/tutorial/quick-start
+https://reactjs.org/
+https://www.electronforge.io/
+https://jojozhuang.github.io/tutorial/converting-web-app-to-desktop-app-with-electron/
---
+++ Source/Web GUI Testing/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Common on MacOS to need to use Cmd + Q to quit
+// On all other OS this will auto quit when the window is closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
