d85dd1ea699aa2ea18481317b2d68c8d1725a398
ApBertran <ray200606@gmail.com>
2023-01-26 20:58:11 -0800

    multiple pages now clickable

--- Source/main.js
+++ Source/main.js
@@ -3,8 +3,7 @@ const path = require('path')
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    fullscreen: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -26,6 +25,3 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
