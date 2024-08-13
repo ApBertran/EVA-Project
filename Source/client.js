875b8bcdbb5e430fbe34a4f9bf32a108d054aff8
ApBertran <ray200606@gmail.com>
2024-08-12 17:20:25 -0700

    g-force gauge first steps

--- Source/client.js
+++ Source/client.js
@@ -11,12 +11,12 @@ socket.on('gforce-update', (gForceArray) => {
   const gForceText = document.getElementById('g-force-text');
 
   // Update needle rotations
-  needleX.style.transform = `rotate(${data.x * 90}deg)`;
-  needleY.style.transform = `rotate(${data.y * 90}deg)`;
-  needleZ.style.transform = `rotate(${data.z * 90}deg)`;
+  needleX.style.transform = `rotate(${gForceArray[0] * 90}deg)`;
+  needleY.style.transform = `rotate(${gForceArray[1] * 90}deg)`;
+  needleZ.style.transform = `rotate(${gForceArray[2] * 90}deg)`;
 
   // Calculate total g-force and display it
-  const totalGForce = Math.sqrt(data.x ** 2 + data.y ** 2 + data.z ** 2).toFixed(2);
+  const totalGForce = Math.sqrt(gForceArray[0] ** 2 + gForceArray[1] ** 2 + gForceArray[2] ** 2).toFixed(2);
   gForceText.innerText = `${totalGForce} G`;
 
   // console.log('Received data:', gForceArray);
